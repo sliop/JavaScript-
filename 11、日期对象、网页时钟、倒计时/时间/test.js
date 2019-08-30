35f202b8f8f34947ff5b3b2d4df87d36e5670551
mac <1045566512@qq.com>
2019-08-30 17:01:11 +0800

    8.30 1

--- 11、日期对象、网页时钟、倒计时/时间/test.js
+++ 11、日期对象、网页时钟、倒计时/时间/test.js
@@ -7,100 +7,55 @@ window.onload = function() {
     var aImg2 = oUl2.getElementsByTagName('img');
     var str = [];
     var aImg = [aImg1, aImg2];
-    // var timer = null;
+    var timer = null;
 
-    fnTime(0);
-    setTimeout(function() {
-        fnTime(1);
-    }, 1000);
-    // clearInterval(timer);
-    // timer = setInterval(function() {
-    //     fnTab(5);
-    // }, 1000);
-    setInterval(function() {
+    
+    fnTime(0, 0, 1, 2, 3, 4, 5, 6, 7);
+    clearInterval(timer);
+    timer = setInterval(function() {
+        fnTab(7);
         fnTab(5);
+        fnTab(2);
+        for (var i = 0; i < str[0].length - 1; i++) {
+            if(str[0][i] != str[1][i]) {
+                fnTab(i);
+            }
+        }
     }, 1000);
+    
+    
 
     function fnTime(n) {
         var now = new Date();
         var iHour = now.getHours();
         var iMin = now.getMinutes();
         var iSec = now.getSeconds();
-        str[n] = toTwo(iHour) + toTwo(iMin) + toTwo(iSec);
-        for (var i = 0; i < str[n].length; i++) {
-            if (i < 2) {
-                aImg[n][i].src = 'img/' + str[n].charAt(i) + '.png';
-            } else if(i < 4) {
-                aImg[n][i + 1].src = 'img/' + str[n].charAt(i) + '.png';
-            } else {
-                aImg[n][i + 2].src = 'img/' + str[n].charAt(i) + '.png';
-            }
+        str[n] = toTwo(iHour) + ':' + toTwo(iMin) + ':' + toTwo(iSec);
+        for(var i = 1; i < arguments.length; i++) {
+            aImg[n][arguments[i]].src = 'img/' + (isNaN(Number(str[n].charAt(arguments[i]))) ? 'colon' : str[n].charAt(arguments[i])) + '.png';
         }
     }
     function fnTab(i) {
-        if (i < 2) {
+        if (isNaN(Number(str[0].charAt(i)))) {
+            aLi1[i].style.display = aLi1[i].style.display == 'none' ? 'inline-block' : 'none';
+        } else {
             if (parseInt(getStyle(aLi1[i], 'top')) == -50) {
                 aLi1[i].style.top = 50 + 'px';
             } else if (parseInt(getStyle(aLi2[i], 'top')) == -100) {
                 aLi2[i].style.top = 0;
             }
+            if (parseInt(getStyle(aLi1[i], 'top')) == 50) {
+                fnTime(0, i);
+            } else if (parseInt(getStyle(aLi2[i], 'top')) == 0) {
+                fnTime(1, i);
+            }
             if (parseInt(getStyle(aLi1[i], 'top')) == 0) {
-                move(aLi1[i], 'top', 1, -50, function() {
-                    setTimeout(function() {
-                        fnTime(0);
-                    }, 500)
-                });
+                move(aLi1[i], 'top', 1, -50);
                 move(aLi2[i], 'top', 1, -50);
             } else if (parseInt(getStyle(aLi1[i], 'top')) == 50) {
                 move(aLi1[i], 'top', 1, 0);
-                move(aLi2[i], 'top', 1, -100, function() {
-                    setTimeout(function() {
-                        fnTime(1);
-                    }, 500)
-                });
+                move(aLi2[i], 'top', 1, -100);
             } 
-        } else if (i < 4) {
-            if (parseInt(getStyle(aLi1[i + 1], 'top')) == -50) {
-                aLi1[i + 1].style.top = 50 + 'px';
-            } else if (parseInt(getStyle(aLi2[i + 1], 'top')) == -100) {
-                aLi2[i + 1].style.top = 0;
-            }
-            if (parseInt(getStyle(aLi1[i + 1], 'top')) == 0) {
-                move(aLi1[i + 1], 'top', 1, -50, function() {
-                    setTimeout(function() {
-                        fnTime(0);
-                    }, 500)
-                });
-                move(aLi2[i + 1], 'top', 1, -50);
-            } else if (parseInt(getStyle(aLi1[i + 1], 'top')) == 50) {
-                move(aLi1[i + 1], 'top', 1, 0);
-                move(aLi2[i + 1], 'top', 1, -100, function() {
-                    setTimeout(function() {
-                        fnTime(1);
-                    }, 500)
-                });
-            }
-        } else {
-            if (parseInt(getStyle(aLi1[i + 2], 'top')) == -50) {
-                aLi1[i + 2].style.top = 50 + 'px';
-            } else if (parseInt(getStyle(aLi2[i + 2], 'top')) == -100) {
-                aLi2[i + 2].style.top = 0;
-            }
-            if (parseInt(getStyle(aLi1[i + 2], 'top')) == 0) {
-                move(aLi1[i + 2], 'top', 1, -50, function() {
-                    setTimeout(function() {
-                        fnTime(0);
-                    }, 500)
-                });
-                move(aLi2[i + 2], 'top', 1, -50);
-            } else if (parseInt(getStyle(aLi1[i + 2], 'top')) == 50) {
-                move(aLi1[i + 2], 'top', 1, 0);
-                move(aLi2[i + 2], 'top', 1, -100, function() {
-                    setTimeout(function() {
-                        fnTime(1);
-                    }, 500)
-                });
-            }
         }
     }
     function move(obj, attr, speed, destination, endFn) {
